7dbf65d5f952af0b1e1ce50814681a5313b30555
v.zaharov <v.zaharov@zarplata.ru>
2023-02-26 16:43:22 +0700

    Написали приложение

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
+import { useState } from "react";
 import "./app.css";
 
+const DEFAULT_HEADER = "Greetings!";
+
 function App() {
+  const [inputValue, setinputValue] = useState('');
+  const [headerValue, setheaderValue] = useState(DEFAULT_HEADER);
+
+  const handleInputChange = (event) => {
+    setinputValue(event.target.value);
+  }
+
+  const handleSubmit = () => {
+    setheaderValue(inputValue);
+    setinputValue('');
+  }
+
+  const handleReset = () => {
+    setheaderValue(DEFAULT_HEADER);
+    setinputValue('');
+  }
+
   return (
     <div className="app">
-      <h1>Greetings</h1>
-      <input type="text" />
-      <button>Отправить</button>
-      <button>Сбросить</button>
+      <h1>{headerValue}</h1>
+      <input type="text" value={inputValue} onChange={handleInputChange} />
+      <button onClick={handleSubmit}>Отправить</button>
+      <button onClick={handleReset}>Сбросить</button>
     </div>
   );
 }
