6559c8f3191de2055a490d2ff41579c8436c0163
v.zaharov <v.zaharov@zarplata.ru>
2023-02-26 15:15:04 +0700

    Пустое прложение без логики

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,12 @@
-import { useState } from "react";
 import "./app.css";
 
-export const DEFAULT_HEADER = "Greetings!";
-
 function App() {
-  const [headerValue, setHeaderValue] = useState(DEFAULT_HEADER);
-  const [inputValue, setInputValue] = useState("");
-
-  const handleInputChange = (e) => {
-    setInputValue(e.target.value);
-  };
-
-  const handleSubmit = () => {
-    setHeaderValue(inputValue);
-    handleClearInput();
-  };
-
-  const handleClearInput = () => {
-    setInputValue("");
-  };
-
-  const handleReset = () => {
-    setHeaderValue(DEFAULT_HEADER);
-    handleClearInput();
-  };
-
   return (
     <div className="app">
-      <h1>{headerValue}</h1>
-      <input type="text" value={inputValue} onChange={handleInputChange} />
-      <button onClick={handleSubmit}>Отправить</button>
-      <button onClick={handleReset}>Сбросить</button>
+      <h1>Greetings</h1>
+      <input type="text" />
+      <button>Отправить</button>
+      <button>Сбросить</button>
     </div>
   );
 }
